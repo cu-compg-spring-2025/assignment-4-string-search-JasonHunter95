faf0e28686861c0ce7ca17c57fe76217bf0c984d
Jason Hunter <jahu8701@colorado.edu>
2025-02-17 19:05:26 -0700

    updated arguments and implemented experiments

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/08twE9R9)
 # string_search
-Empirical comparison of string search methods.
+Empirical comparison of string search methods (Naive vs Boyer-Moore) on different types of text.
 
 ## Usage
 ```
-usage: string_search.py [-h] --text_range TEXT_RANGE TEXT_RANGE TEXT_RANGE
-                        --pattern_size PATTERN_SIZE [--rounds ROUNDS]
-                        --out_file OUT_FILE [--width WIDTH]
-                        [--height HEIGHT]
+usage: string_search.py [-h] [
+--experiment_type {Nucleotides, Alphabet, RepeatingChars}]  
+--text_range TEXT_RANGE TEXT_RANGE TEXT_RANGE  
+--pattern_size PATTERN_SIZE [--rounds ROUNDS]  
+--out_file OUT_FILE [--width WIDTH] [--height HEIGHT]
 
 optional arguments:
   -h, --help            show this help message and exit
+  --experiment_type {Nucleotides, Alphabet, RepeatingChars}
+                        Type of experiment to run (default: Nucleotides)
   --text_range TEXT_RANGE TEXT_RANGE TEXT_RANGE
                         Text size parameters (start stop step)
   --pattern_size PATTERN_SIZE
@@ -21,12 +24,34 @@ optional arguments:
   --height HEIGHT       Height of plot in inches (default: 5)
 ```
 
-## Examles
+## Examples
 ```
-python src/string_search.py \
+python3 src/string_search.py \
+    --experiment_type RepeatingChars \      
     --text_range 100 10000 100 \
     --pattern_size 100 \
-    --rounds 1 \
-    --out_file doc/naive_search.png
+    --rounds 10 \
+    --out_file doc/worstcase_search.png
 ```
-<center><img src="doc/naive_search.png" width="600"/></center>
+<center><img src="doc/worstcase_search.png" width="600"/></center>
+
+```
+python3 src/string_search.py \
+    --experiment_type Alphabet \      
+    --text_range 100 10000 100 \
+    --pattern_size 100 \
+    --rounds 10 \
+    --out_file doc/alphabet_search.png
+```
+<center><img src="doc/alphabet_search.png" width="600"/></center>
+
+```
+python3 src/string_search.py \
+    --experiment_type Nucleotides \      
+    --text_range 100 10000 100 \
+    --pattern_size 100 \
+    --rounds 10 \
+    --out_file doc/nucleotide_search.png
+```
+<center><img src="doc/nucleotide_search.png" width="600"/></center>
+
--- src/string_search.py
+++ src/string_search.py
@@ -9,6 +9,11 @@ import boyer_moore
 
 def get_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--experiment_type',
+                        type=str,
+                        default='Nucleotides',
+                        choices=['Nucleotides', 'Alphabet', 'RepeatingChars'],
+                        help='Type of experiment to run')
     parser.add_argument('--text_range',
                         type=int,
                         required=True,
@@ -62,7 +67,8 @@ def run_test(test_function, T, P):
 def test_harness(test_functions,
                  text_size_range,
                  pattern_size,
-                 rounds):
+                 rounds,
+                 experiment_type='Nucleotides'):
 
     run_times = [ [] for _ in range(len(test_functions))]
     mem_usages = [ [] for _ in range(len(test_functions))]
@@ -73,9 +79,19 @@ def test_harness(test_functions,
         _mem_usages = [ [] for _ in range(len(test_functions))]
 
         for i in range(rounds):
-            T = get_random_string(['A', 'C', 'T', 'G'], text_size)
-            P = get_random_substring(T, pattern_size)
-
+            if experiment_type == 'Nucleotides':
+                T = get_random_string(['A', 'C', 'T', 'G'], text_size)
+                P = get_random_substring(T, pattern_size)
+            elif experiment_type == 'Alphabet':
+                T = get_random_string(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+                                       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{',
+                                        '}', '[', ']', '|', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '/', '?', '`', '~'
+                                       ], text_size)
+                P = get_random_substring(T, pattern_size)
+            elif experiment_type == 'RepeatingChars':
+                T = 'A' * text_size
+                P = 'A' * pattern_size
             for j, test_function in enumerate(test_functions):
                 run_time, mem_usage = run_test(test_function, T, P)
                 _run_times[j].append(run_time)
@@ -99,13 +115,16 @@ def main():
     run_times, mem_usages = test_harness(test_functions,
                                          text_size_range,
                                          args.pattern_size,
-                                         args.rounds)
+                                         args.rounds,
+                                         experiment_type=args.experiment_type)
 
     fig, axs = plt.subplots(2,1, figsize=(10, 12), sharex=False, sharey=False)
     ax = axs[0]
     ax.plot(text_size_range, run_times[0], label='Naive', color='red')
     ax.plot(text_size_range, run_times[1], label='Boyer-Moore', color='blue')
-    ax.set_title(f'String Search Performance(|P|= {args.pattern_size})', fontsize=16)
+    ax.set_title(f'String Search Performance (|Pattern|= {args.pattern_size}) on ' \
+                 +
+                 f'{args.experiment_type})', fontsize=16)
     ax.set_xlabel('Text size', fontsize=14)
     ax.set_ylabel('Run time (ns)', fontsize=14)
     ax.legend(loc='best', frameon=False, ncol=3)
@@ -122,25 +141,6 @@ def main():
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.grid(True, linestyle='--', alpha=0.6)
-
-    
-    # ax = axs[2]
-    # ax.plot(text_size_range, run_times[1], label='Boyer-Moore')
-    # ax.set_xlabel('Text size', fontsize=14)
-    # ax.set_ylabel('Run time (ns)', fontsize=14)
-    # ax.legend(loc='best', frameon=False, ncol=3)
-    # ax.spines['top'].set_visible(False)
-    # ax.spines['right'].set_visible(False)
-    # ax.grid(True, linestyle='--', alpha=0.6)
-    
-    # ax = axs[3]
-    # ax.plot(text_size_range, mem_usages[1], label='Boyer-Moore')
-    # ax.set_xlabel('Text size', fontsize=14)
-    # ax.set_ylabel('Memory (bytes)', fontsize=14)
-    # ax.legend(loc='best', frameon=False, ncol=3)
-    # ax.spines['top'].set_visible(False)
-    # ax.spines['right'].set_visible(False)
-    # ax.grid(True, linestyle='--', alpha=0.6)
     
     plt.tight_layout()
 
